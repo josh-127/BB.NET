7510ac56c13e9f40c6acbe12933548fbde21e081
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 13:20:12 -0500

    Add topic and forum service interfaces

---
+++ BBNet.Data/IForumService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BBNet.Data
+{
+    public interface IForumService
+    {
+        Forum GetForumById(int forumId);
+
+        IEnumerable<Forum> GetAllForums();
+
+        void AddForum(Forum forum);
+    }
+}
---
+++ BBNet.Data/ITopicService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BBNet.Data
+{
+    public interface ITopicService
+    {
+        Topic GetTopicById(int topicId);
+
+        IEnumerable<Topic> GetAllTopics();
+
+        IEnumerable<Topic> GetTopicsByForumId(int forumId);
+
+        void AddTopic(Topic topic);
+    }
+}
