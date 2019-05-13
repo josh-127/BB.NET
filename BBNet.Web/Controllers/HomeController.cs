9713ab8ad39b0708a705e7c29b6ff083b87c36e5
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 13:26:07 -0500

    Implement /Home/Index controller action

--- BBNet.Web/Controllers/HomeController.cs
+++ BBNet.Web/Controllers/HomeController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBNet.Data;
+using BBNet.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BBNet.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IForumService forumService;
+
+        public HomeController(IForumService forumService)
+            => this.forumService = forumService;
+
         public IActionResult Index()
         {
-            return View();
+            var forums = forumService.GetAllForums();
+
+            var forumListings = from f in forums
+                                select new ForumListingViewModel
+                                {
+                                    Id = f.Id,
+                                    Name = f.Name,
+                                    Description = f.Description,
+                                    ImageUrl = f.ImageUrl,
+                                };
+
+            var viewModel = new HomeIndexViewModel
+            {
+                Forums = forumListings
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ BBNet.Web/ViewModels/ForumListingViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BBNet.Web.ViewModels
+{
+    public class ForumListingViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
