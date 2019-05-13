bd08b48e111e43209345a1441b6c273e4ee7e2fe
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 13:28:39 -0500

    Implement IForumService

---
+++ BBNet.Data/ForumService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BBNet.Data
+{
+    public class ForumService : IForumService
+    {
+        private readonly BBNetDbContext context;
+
+        public ForumService(BBNetDbContext context)
+            => this.context = context;
+
+        public IEnumerable<Forum> GetAllForums()
+            => context.Forums;
+
+        public Forum GetForumById(int forumId)
+            => context.Forums
+                .Where(f => f.Id == forumId)
+                .SingleOrDefault();
+
+        public void AddForum(Forum forum)
+        {
+            context.Forums.Add(forum);
+            context.SaveChanges();
+        }
+    }
+}
