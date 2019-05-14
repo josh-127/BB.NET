09a5ceba83d9ed83f7260b04aecff19306e26bc3
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 19:42:24 -0500

    Run Entity Framework migrations

---
+++ BBNet.Data/Migrations/20190514004131_FixTopicTypo.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBNet.Data.Migrations
+{
+    public partial class FixTopicTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Topic_TopicId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topic_Forums_ForumId",
+                table: "Topic");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Topic",
+                table: "Topic");
+
+            migrationBuilder.RenameTable(
+                name: "Topic",
+                newName: "Topics");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Topic_ForumId",
+                table: "Topics",
+                newName: "IX_Topics_ForumId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Topics",
+                table: "Topics",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Topics_TopicId",
+                table: "Posts",
+                column: "TopicId",
+                principalTable: "Topics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_Forums_ForumId",
+                table: "Topics",
+                column: "ForumId",
+                principalTable: "Forums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Topics_TopicId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_Forums_ForumId",
+                table: "Topics");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Topics",
+                table: "Topics");
+
+            migrationBuilder.RenameTable(
+                name: "Topics",
+                newName: "Topic");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Topics_ForumId",
+                table: "Topic",
+                newName: "IX_Topic_ForumId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Topic",
+                table: "Topic",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Topic_TopicId",
+                table: "Posts",
+                column: "TopicId",
+                principalTable: "Topic",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topic_Forums_ForumId",
+                table: "Topic",
+                column: "ForumId",
+                principalTable: "Forums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
