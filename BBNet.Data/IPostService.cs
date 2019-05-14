80f41c2d0614561528fc4faa6b5f48d1de673da0
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-14 02:23:55 -0500

    Add and implement IPostService

---
+++ BBNet.Data/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BBNet.Data
+{
+    public interface IPostService
+    {
+        Post GetPostById(int postId);
+
+        IEnumerable<Post> GetAllPosts();
+
+        IEnumerable<Post> GetPostsByTopicId(int topicId);
+
+        void AddPost(Post post, Topic topic);
+    }
+}
---
+++ BBNet.Data/PostService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BBNet.Data
+{
+    public class PostService : IPostService
+    {
+        private readonly BBNetDbContext context;
+
+        public PostService(BBNetDbContext context)
+            => this.context = context;
+
+        public Post GetPostById(int postId)
+            => context.Posts
+                .Where(p => p.Id == postId)
+                .SingleOrDefault();
+
+        public IEnumerable<Post> GetAllPosts()
+            => context.Posts;
+
+        public IEnumerable<Post> GetPostsByTopicId(int topicId)
+            => context.Posts
+                .Where(p => p.Topic.Id == topicId);
+
+        public void AddPost(Post post, Topic topic)
+        {
+            post.Topic = topic;
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
