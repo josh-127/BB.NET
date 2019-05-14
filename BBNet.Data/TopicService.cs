8b88f003b8714799d69ea5483acc0e88ef8f4a8c
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 19:42:45 -0500

    Implement ITopicService

---
+++ BBNet.Data/TopicService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BBNet.Data
+{
+    public class TopicService : ITopicService
+    {
+        private readonly BBNetDbContext context;
+
+        public TopicService(BBNetDbContext context)
+            => this.context = context;
+
+        public Topic GetTopicById(int topicId)
+            => context.Topics
+                .Where(t => t.Id == topicId)
+                .SingleOrDefault();
+
+        public IEnumerable<Topic> GetAllTopics()
+            => context.Topics;
+
+        public IEnumerable<Topic> GetTopicsByForumId(int forumId)
+            => context.Topics.Where(t => t.Forum.Id == forumId);
+
+        public void AddTopic(Topic topic)
+        {
+            context.Topics.Add(topic);
+            context.SaveChanges();
+        }
+    }
+}
