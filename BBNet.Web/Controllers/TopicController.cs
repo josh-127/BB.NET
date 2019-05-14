83659b2678f94a321f94413be126a095eafb65a7
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 21:13:40 -0500

    Add /Topic/* page stubs

---
+++ BBNet.Web/Controllers/TopicController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBNet.Web.Controllers
+{
+    public class TopicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
