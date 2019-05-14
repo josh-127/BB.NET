4742c1324d302d08c301ea868335688330172b30
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-13 20:08:40 -0500

    Add /Admin/Forums page stubs

--- BBNet.Web/Controllers/AdminController.cs
+++ BBNet.Web/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BBNet.Data;
+using BBNet.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,29 @@ namespace BBNet.Web.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IForumService forumService;
+
+        public AdminController(IForumService forumService)
+            => this.forumService = forumService;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Forums()
+        {
+            var forums = forumService.GetAllForums();
+
+            var forumListings = from f in forums
+                                select f.ToForumListing();
+
+            var viewModel = new AdminForumsViewModel
+            {
+                Forums = forumListings
+            };
+
+            return View(viewModel);
         }
     }
 }
