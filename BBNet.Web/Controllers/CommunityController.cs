5e2b0949f1602aca8f3268c0cb19e7285607546d
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-15 05:27:11 -0500

    Implement /Community/Index page

---
+++ BBNet.Web/Controllers/CommunityController.cs
@@ -0,0 +1,31 @@
+﻿using BBNet.Data;
+using BBNet.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BBNet.Web.Controllers
+{
+    public class CommunityController : Controller
+    {
+        private readonly ForumService forumService;
+
+        public CommunityController(ForumService forumService)
+            => this.forumService = forumService;
+
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            var forums = forumService.GetForumsByCommunityId(id);
+
+            var forumListings = from f in forums
+                                select f.ToForumListing();
+
+            var viewModel = new CommunityIndexViewModel
+            {
+                Forums = forumListings
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BBNet.Web/ViewModels/CommunityIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BBNet.Web.ViewModels
+{
+    public class CommunityIndexViewModel
+    {
+        public IEnumerable<ForumListingViewModel> Forums { get; set; }
+    }
+}
