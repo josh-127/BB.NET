3a675e142d5f6e3979d8c170123e0b33acbf27a9
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-15 05:34:19 -0500

    Add community services

---
+++ BBNet.Data/CommunityService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BBNet.Data
+{
+    public class CommunityService
+    {
+        private readonly BBNetDbContext context;
+
+        public CommunityService(BBNetDbContext context)
+            => this.context = context;
+
+        public IEnumerable<Community> GetAllCommunities()
+            => context.Communities;
+
+        public Community GetCommunityById(int communityId)
+            => context.Communities
+                .Where(c => c.Id == communityId)
+                .SingleOrDefault();
+
+        public void AddCommunity(Community community)
+        {
+            context.Communities.Add(community);
+            context.SaveChanges();
+        }
+    }
+}
