393ea33ebba0ad28a19536923fbf6eefaf4c1162
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-25 02:14:34 -0500

    Create new schema

--- schema.sql
+++ schema.sql
@@ -5,102 +5,95 @@ CREATE DATABASE IF NOT EXISTS `BBNet`
 
 USE BBNet;
 
-CREATE TABLE IF NOT EXISTS `User` (
-    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
-    `Email`         VARCHAR(79)     NOT NULL,
-    `UserName`      VARCHAR(79)     NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS `Role` (
+CREATE TABLE IF NOT EXISTS `Category` (
     `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
     `Name`          VARCHAR(79)     NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `Permission` (
-    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
-    `RoleId`        INT             NOT NULL    REFERENCES `Role`
-#    `Type`          ENUM ( ?? )    NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS `Community` (
-    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
-    `Name`          VARCHAR(79)     NOT NULL,
-    `Description`   VARCHAR(255)    NOT NULL,
-    `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS `Forum` (
     `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `CategoryId`    INT             NOT NULL    REFERENCES `Category`,
     `Name`          VARCHAR(79)     NOT NULL,
-    `Description`   VARCHAR(255)    NOT NULL,
-    `ImageUrl`      VARCHAR(255)    NOT NULL,
-    `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    `CommunityId`   INT             NOT NULL    REFERENCES `Community`
+    `Description`   VARCHAR(255),
+    `ImageUrl`      VARCHAR(255),
+    `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `Topic` (
     `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
-    `Name`          VARCHAR(79)     NOT NULL,
-    `Description`   VARCHAR(79)     NOT NULL,
-    `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     `ForumId`       INT             NOT NULL    REFERENCES `Forum`,
-    `UserId`        INT             NOT NULL    REFERENCES `User`
+    `Name`          VARCHAR(79)     NOT NULL,
+    `Description`   VARCHAR(255),
+    `TopicBannerId` INT                         REFERENCES `TopicBanner`,
+    `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    `IsLocked`      BOOL            NOT NULL,
+    `IsSticky`      BOOL            NOT NULL,
+    `Views`         INT             NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `TopicBanner` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `ImageUrl`      VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Post` (
     `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `TopicId`       INT             NOT NULL    REFERENCES `Topic`,
     `Name`          VARCHAR(79)     NOT NULL,
     `Body`          TEXT            NOT NULL,
     `Created`       TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    `Modified`      TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `Group` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `Name`          VARCHAR(79)     NOT NULL,
+    `Description`   VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `User` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `GroupId`       INT             NOT NULL    REFERENCES `Group`,
+    `Email`         VARCHAR(79)     NOT NULL,
+    `UserName`      VARCHAR(79)     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `GroupPermission` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `GroupId`       INT             NOT NULL    REFERENCES `Group`
+#    `Type`          ENUM            NOT NULL,
+#    `Policy`        ENUM            NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `UserPermission` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `UserId`        INT             NOT NULL    REFERENCES `User`
+#    `Type`          ENUM            NOT NULL,
+#    `Policy`        ENUM            NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `ForumGroupPermission` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `ForumId`       INT             NOT NULL    REFERENCES `Forum`,
+    `GroupId`       INT             NOT NULL    REFERENCES `Group`
+#    `Type`          ENUM            NOT NULL,
+#    `Policy`        ENUM            NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `ForumUserPermission` (
+    `Id`            INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+    `ForumId`       INT             NOT NULL    REFERENCES `Forum`,
+    `UserId`        INT             NOT NULL    REFERENCES `User`
+#    `Type`          ENUM            NOT NULL,
+#    `Policy`        ENUM            NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `UnreadTopic` (
     `TopicId`       INT             NOT NULL    REFERENCES `Topic`,
     `UserId`        INT             NOT NULL    REFERENCES `User`
 );
 
-CREATE TABLE IF NOT EXISTS `UserCommunity` (
-    `UserId`        INT     NOT NULL REFERENCES `User`,
-    `CommunityId`   INT     NOT NULL REFERENCES `Community`
+CREATE TABLE IF NOT EXISTS `PostLike` (
+    `PostId`        INT             NOT NULL    REFERENCES `Post`,
+    `UserId`        INT             NOT NULL    REFERENCES `User`
 );
-
-CREATE TABLE IF NOT EXISTS `UserForum` (
-    `UserId`        INT     NOT NULL REFERENCES `User`,
-    `ForumId`       INT     NOT NULL    REFERENCES `Forum`
-);
-
-DELIMITER $$
-CREATE PROCEDURE CreateCommunity(IN `name` VARCHAR(79), IN `description` VARCHAR(255))
-BEGIN
-    INSERT INTO `Community` (`Name`, `Description`) VALUES (`name`, `description`);
-END $$
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE GetCommunityListings()
-BEGIN
-    SELECT `Id`, `Name`, `Description`, `Created` FROM `Community`;
-END $$
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE CreateForum(
-    IN `name` VARCHAR(79),
-    IN `description` VARCHAR(255),
-    IN `imageUrl` VARCHAR(255),
-    IN `communityId` INT)
-BEGIN
-    INSERT INTO `Forum` (`Name`, `Description`, `ImageUrl`, `CommunityId`)
-        VALUES (`name`, `description`, `imageUrl`, `communityId`);
-END $$
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE GetForumIndex(IN `communityId` INT)
-BEGIN
-    SELECT `Name`, `Description`
-        FROM `Community`
-        WHERE `Id` = `communityId`;
-
-    SELECT `Id`, `Name`, `Description`, `ImageUrl`
-        FROM `Forum`
-        WHERE `CommunityId` = `communityId`;
-END $$
-DELIMITER ;
