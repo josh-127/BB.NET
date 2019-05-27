f921822b984c869e5c73a40a5e5f9a195d522804
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-27 01:08:10 -0500

    Add IModel trait

---
+++ BBNet.Service/Models/IModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BBNet.Service.Models
+{
+    public interface IModel { }
+
+    public static class IModelExtensions
+    {
+        public static ValidationResultCollection GetValidationResult(this IModel model)
+        {
+            var context = new ValidationContext(model);
+            var results = new ValidationResultCollection();
+
+            Validator.TryValidateObject(model, context, results);
+            return results;
+        }
+
+        public static bool IsValid(this IModel model)
+            => model.GetValidationResult().IsSuccessful;
+    }
+}
