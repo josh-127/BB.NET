82a4b82ba16b6a1f785c8c1eff2a6a6e602cf8a3
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-27 01:25:49 -0500

    Add NoWhitespaceAttribute

---
+++ BBNet.Service/Models/NoWhitespaceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BBNet.Service.Models
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class NoWhitespaceAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string s)
+            {
+                if (s.Contains(" ") || s.Contains("\t") || s.Contains("\r") || s.Contains("\n"))
+                    return new ValidationResult("Property contains whitespace.");
+
+                return null;
+            }
+
+            return new ValidationResult("Property must be a string.");
+        }
+    }
+}
