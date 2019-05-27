345263d0e0bd1fe847d688c2a3b796cb35775f55
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-27 00:11:54 -0500

    Add UserService and user registration stubs

---
+++ BBNet.Service/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BBNet.Service.Models;
+using Tortuga.Chain;
+
+namespace BBNet.Service
+{
+    public class UserService
+    {
+        private readonly MySqlDataSource dataSource;
+
+        public UserService(MySqlDataSource dataSource)
+            => this.dataSource = dataSource;
+
+        public void RegisterUser(UserRegistrationModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
