ebe08e504084eacc078cf53ec99c9d4ca7f4f478
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-27 00:59:51 -0500

    Add extension methods for model validation

---
+++ BBNet.Service/Models/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BBNet.Service.Models
+{
+    public static class ObjectExtensions
+    {
+        public static ValidationResultCollection GetValidationResult(this object instance)
+        {
+            var context = new ValidationContext(instance);
+            var results = new ValidationResultCollection();
+
+            Validator.TryValidateObject(instance, context, results);
+            return results;
+        }
+    }
+}
---
+++ BBNet.Service/Models/ValidationResultCollection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BBNet.Service.Models
+{
+    public sealed class ValidationResultCollection : ICollection<ValidationResult>
+    {
+        private readonly List<ValidationResult> list = new List<ValidationResult>();
+
+        public bool IsSuccessful => list.Count == 0;
+
+        public int Count => list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(ValidationResult item)
+            => list.Add(item);
+
+        public void Clear()
+            => list.Clear();
+
+        public bool Contains(ValidationResult item)
+            => list.Contains(item);
+
+        public void CopyTo(ValidationResult[] array, int arrayIndex)
+            => list.CopyTo(array, arrayIndex);
+
+        public IEnumerator<ValidationResult> GetEnumerator()
+            => list.GetEnumerator();
+
+        public bool Remove(ValidationResult item)
+            => list.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => list.GetEnumerator();
+    }
+}
