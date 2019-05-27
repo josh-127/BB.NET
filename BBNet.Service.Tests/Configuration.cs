c17bd82cc6c800e91f01f86d2a1e98f1f6f9522a
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-27 00:23:06 -0500

    Fix invalid MySQL connection string

--- BBNet.Service.Tests/Configuration.cs
+++ BBNet.Service.Tests/Configuration.cs
@@ -3,6 +3,6 @@ namespace BBNet.Service.Tests
 {
     public static class Configuration
     {
-        public const string ConnectionString = "root@localhost:3306";
+        public const string ConnectionString = "server=127.0.0.1;port=3306;database=bbnet;uid=root";
     }
 }
