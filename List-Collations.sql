b2fdddfc03252d52ea7bc8cdcc09909443891ee1
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-30 04:03:16 -0500

    Switch to MySQL

--- List-Collations.sql
+++
@@ -1,7 +0,0 @@
-
--- Gets the default collation.
-SELECT SERVERPROPERTY('collation');
-
--- Lists all collations.
-SELECT [name] AS [Name], COLLATIONPROPERTY(name, 'CodePage') AS [Code Page], [description] AS [Description]
-    FROM sys.fn_helpcollations();
