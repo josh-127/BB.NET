9dd03074deb0f4a636225d13c3a83317e0199135
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-30 03:17:32 -0500

    Update List-Collations.sql

--- List-Collations.sql
+++ List-Collations.sql
@@ -1,3 +1,7 @@
 
-SELECT name AS [Name], COLLATIONPROPERTY(name, 'CodePage') AS [Code Page], description AS [Description]
+-- Gets the default collation.
+SELECT SERVERPROPERTY('collation');
+
+-- Lists all collations.
+SELECT [name] AS [Name], COLLATIONPROPERTY(name, 'CodePage') AS [Code Page], [description] AS [Description]
     FROM sys.fn_helpcollations();
