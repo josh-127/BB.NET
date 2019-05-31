1d07cda90a2e0030334b1b03ad983dd94f8039bb
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-05-31 18:57:18 -0500

    Add cookie authentication and a log-in page

---
+++ BBNet.Web/Models/LogInModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace BBNet.Web.Models
+{
+    public class LogInModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [HiddenInput]
+        public string ReturnUrl { get; set; }
+    }
+}
--- BBNet.Web/Startup.cs
+++ BBNet.Web/Startup.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(BBNet.Web.Startup))]
@@ -13,6 +10,12 @@ namespace BBNet.Web
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+                LoginPath = new PathString("/Account/LogIn"),
+                LogoutPath = new PathString("/Account/LogOut")
+            });
         }
     }
 }
