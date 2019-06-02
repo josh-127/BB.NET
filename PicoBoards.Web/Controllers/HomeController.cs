7ed75c4cb97c15cb9512a99d3cef73df5e3ba4a7
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-01 21:27:58 -0500

    Add /Home/Users page

--- PicoBoards.Web/Controllers/HomeController.cs
+++ PicoBoards.Web/Controllers/HomeController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PicoBoards.Services;
 
 namespace PicoBoards.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserService userService;
+
+        public HomeController(UserService userService)
+            => this.userService = userService;
+
         public IActionResult Index() => View();
+
+        [HttpGet]
+        public async Task<IActionResult> Users()
+        {
+            var model = await userService.GetUserListings();
+            return View(model);
+        }
     }
 }
