7023667e5cfe2877192c70b0dea7ccf3f0deb95b
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-02 15:59:39 -0500

    Add Result<TDerived, TValue, TError> type

---
+++ PicoBoards/Result.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PicoBoards
+{
+    public abstract class Result<TDerived, TValue, TError>
+        where TDerived : Result<TDerived, TValue, TError>
+    {
+        public TValue Value { get; }
+        public TError Error { get; }
+        public bool IsSuccessful { get; }
+
+        public Result(TValue value)
+            => (Value, IsSuccessful) = (value, true);
+
+        public Result(TError value)
+            => Error = value;
+
+        public TDerived Select(Func<TValue, TDerived> mapper)
+            => IsSuccessful
+            ? mapper(Value)
+            : (TDerived) Activator.CreateInstance(typeof(TDerived), Error);
+
+        public TDerived SelectMany<TIntermediate, TIntermediateDerived>(
+            Func<TValue, Result<TIntermediateDerived, TIntermediate, TError>> mapper,
+            Func<TValue, TIntermediate, TDerived> resultGetter)
+            where TIntermediateDerived : Result<TIntermediateDerived, TIntermediate, TError>
+        {
+            if (IsSuccessful)
+            {
+                var intermediate = mapper(Value);
+                return intermediate.IsSuccessful
+                    ? resultGetter(Value, intermediate.Value)
+                    : (TDerived) Activator.CreateInstance(typeof(TDerived), intermediate.Error);
+            }
+
+            return (TDerived) Activator.CreateInstance(typeof(TDerived), Error);
+        }
+    }
+}
