4edf724c91b7e7fd6e1458af69652a61e9182b6e
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-02 16:33:08 -0500

    Add /Ucp/ChangeEmailAddress page

--- PicoBoards.Web/Controllers/UcpController.cs
+++ PicoBoards.Web/Controllers/UcpController.cs
@@ -1,13 +1,25 @@
 ﻿using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PicoBoards.Models;
+using PicoBoards.Services;
+using PicoBoards.Web.Models;
 
 namespace PicoBoards.Web.Controllers
 {
     public class UcpController : Controller
     {
+        private readonly UserService userService;
+
+        public UcpController(UserService userService)
+            => this.userService = userService;
+
         private IActionResult RedirectToLogin()
             => RedirectToAction("Login", "Auth", new { returnUrl = HttpContext.Request.Path });
+
+        private bool IsAuthenticated => User.Identity.IsAuthenticated;
+
+        private int UserId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
         [HttpGet]
         public IActionResult Index()
@@ -20,9 +32,26 @@ namespace PicoBoards.Web.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> ChangeEmailAddress()
+        {
+            if (!IsAuthenticated)
+                return RedirectToLogin();
+
+            var emailAddress = await userService.GetUserEmailAddress(UserId);
+            return View(new ChangeEmailAddressForm(emailAddress));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeEmailAddress(ChangeEmailAddressForm form)
+        {
+            await userService.SetUserEmailAddress(UserId, form.EmailAddress);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
         public IActionResult AccountSettings()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (!IsAuthenticated)
                 return RedirectToLogin();
 
             return View();
---
+++ PicoBoards.Web/Models/ChangeEmailAddressForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PicoBoards.Web.Models
+{
+    public sealed class ChangeEmailAddressForm
+    {
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public ChangeEmailAddressForm() { }
+
+        public ChangeEmailAddressForm(string emailAddress)
+            => EmailAddress = emailAddress;
+    }
+}
