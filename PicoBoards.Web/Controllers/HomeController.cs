5eadb0e153485a687eff9292da992939eec1bfe6
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-01 21:57:28 -0500

    Move /Home/Users page to /User/Index

--- PicoBoards.Web/Controllers/HomeController.cs
+++ PicoBoards.Web/Controllers/HomeController.cs
@@ -1,23 +1,11 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PicoBoards.Services;
 
 namespace PicoBoards.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UserService userService;
-
-        public HomeController(UserService userService)
-            => this.userService = userService;
-
-        public IActionResult Index() => View();
-
         [HttpGet]
-        public async Task<IActionResult> Users()
-        {
-            var model = await userService.GetUserListings();
-            return View(model);
-        }
+        public IActionResult Index() => View();
     }
 }
---
+++ PicoBoards.Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PicoBoards.Services;
+
+namespace PicoBoards.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService userService;
+
+        public UserController(UserService userService)
+            => this.userService = userService;
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await userService.GetUserListings();
+            return View(model);
+        }
+    }
+}
