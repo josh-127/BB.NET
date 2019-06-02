f2339d22d603146e6c85cd8f47844bdc764f8ecb
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-01 20:00:39 -0500

    Add IVadlidatable trait

---
+++ PicoBoards/IValidatable.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PicoBoards
+{
+    public interface IValidatable { }
+
+    public static class IValidatableExtensions
+    {
+        public static ValidationResultCollection GetValidationResult(this IValidatable validatable)
+        {
+            var context = new ValidationContext(validatable);
+            var results = new ValidationResultCollection();
+
+            Validator.TryValidateObject(validatable, context, results, true);
+            return results;
+        }
+
+        public static bool IsValid(this IValidatable validatable)
+            => validatable.GetValidationResult().IsValid;
+    }
+}
