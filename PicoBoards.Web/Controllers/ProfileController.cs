d48738ebf9054ffbf7efce23e488f3cdddb93242
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-01 22:20:52 -0500

    Implement /Profile/Index/{id} page

--- PicoBoards.Web/Controllers/ProfileController.cs
+++ PicoBoards.Web/Controllers/ProfileController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PicoBoards.Services;
 
 namespace PicoBoards.Web.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly UserService userService;
+
+        public ProfileController(UserService userService)
+            => this.userService = userService;
+
         public async Task<IActionResult> Index(string userName)
         {
-            return View();
+            var model = await userService.GetUserProfile(userName);
+            return View(model);
         }
     }
 }
