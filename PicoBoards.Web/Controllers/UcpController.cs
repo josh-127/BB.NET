143d50a014c10eaf55974303dc92efd168d52425
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-02 13:24:12 -0500

    Add more pages to UcpController

--- PicoBoards.Web/Controllers/UcpController.cs
+++ PicoBoards.Web/Controllers/UcpController.cs
@@ -1,15 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using PicoBoards.Models;
 
 namespace PicoBoards.Web.Controllers
 {
     public class UcpController : Controller
     {
+        private IActionResult RedirectToLogin()
+            => RedirectToAction("Login", "Auth", new { returnUrl = HttpContext.Request.Path });
+
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             if (!User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Auth", new { returnUrl = HttpContext.Request.Path });
+                return RedirectToLogin();
+
+            var model = new Dashboard(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult AccountSettings()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToLogin();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProfileSettings()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToLogin();
 
             return View();
         }
---
+++ PicoBoards/Models/Dashboard.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PicoBoards.Models
+{
+    public class Dashboard
+    {
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        public Dashboard() { }
+
+        public Dashboard(string userName)
+            => UserName = userName;
+    }
+}
