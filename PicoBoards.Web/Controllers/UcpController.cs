f8134af81a6bbe079c5103168bee3eafa59571fe
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-02 12:00:29 -0500

    Add stub for user control panel

---
+++ PicoBoards.Web/Controllers/UcpController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PicoBoards.Web.Controllers
+{
+    public class UcpController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Auth", new { returnUrl = HttpContext.Request.Path });
+
+            return View();
+        }
+    }
+}
