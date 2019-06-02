32ec87687bcf5eab79a8b2be88e7470482561223
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-01 21:59:15 -0500

    Add stubs for the profile page

---
+++ PicoBoards.Web/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PicoBoards.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public async Task<IActionResult> Index(string userName)
+        {
+            return View();
+        }
+    }
+}
