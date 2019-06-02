c7b5b3391457ebd1cd45c39cea118c43d4b38ca4
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-02 01:45:43 -0500

    Add IModel trait

---
+++ PicoBoards/IModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace PicoBoards
+{
+    public interface IModel { }
+
+    public static class IModelExtensions
+    {
+        public static string GetDisplayName(this IModel model, string propertyName)
+            => (model
+                .GetType()
+                .GetProperty(propertyName)
+                .GetCustomAttributes(typeof(DisplayAttribute), false)
+                .First() as DisplayAttribute)
+                .Name;
+    }
+}
