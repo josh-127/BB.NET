66e5fac3c76ef14c116722c4bb221cc2c292c8f3
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-03 04:30:48 -0500

    Merge model and table files

--- PicoBoards/Security/GroupListing.cs
+++ PicoBoards/Security/GroupListing.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+
 namespace PicoBoards.Security
 {
     public class GroupListing
@@ -8,5 +10,41 @@ namespace PicoBoards.Security
         public string Name { get; set; }
 
         public string Description { get; set; }
+    }
+
+    public sealed class GroupListingTable : ICollection<GroupListing>
+    {
+        private readonly List<GroupListing> list;
+
+        public GroupListingTable()
+            => list = new List<GroupListing>();
+
+        public GroupListingTable(IEnumerable<GroupListing> collection)
+            => list = new List<GroupListing>(collection);
+
+        public int Count => list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(GroupListing item)
+            => list.Add(item);
+
+        public void Clear()
+            => list.Clear();
+
+        public bool Contains(GroupListing item)
+            => list.Contains(item);
+
+        public void CopyTo(GroupListing[] array, int arrayIndex)
+            => list.CopyTo(array, arrayIndex);
+
+        public IEnumerator<GroupListing> GetEnumerator()
+            => list.GetEnumerator();
+
+        public bool Remove(GroupListing item)
+            => list.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => list.GetEnumerator();
     }
 }
--- PicoBoards/Security/UserListing.cs
+++ PicoBoards/Security/UserListing.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace PicoBoards.Security
@@ -26,5 +28,41 @@ namespace PicoBoards.Security
 
         [Display(Name = "Topic Count")]
         public int TopicCount { get; set; }
+    }
+
+    public sealed class UserListingTable : ICollection<UserListing>
+    {
+        private readonly List<UserListing> list;
+
+        public UserListingTable()
+            => list = new List<UserListing>();
+
+        public UserListingTable(IEnumerable<UserListing> collection)
+            => list = new List<UserListing>(collection);
+
+        public int Count => list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(UserListing item)
+            => list.Add(item);
+
+        public void Clear()
+            => list.Clear();
+
+        public bool Contains(UserListing item)
+            => list.Contains(item);
+
+        public void CopyTo(UserListing[] array, int arrayIndex)
+            => list.CopyTo(array, arrayIndex);
+
+        public IEnumerator<UserListing> GetEnumerator()
+            => list.GetEnumerator();
+
+        public bool Remove(UserListing item)
+            => list.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => list.GetEnumerator();
     }
 }
