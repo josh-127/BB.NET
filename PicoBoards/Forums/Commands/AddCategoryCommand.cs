432011171baca8345a3f89812e594b162f6069c9
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-03 15:09:50 -0500

    Add ForumService and AddCategoryCommand

---
+++ PicoBoards/Forums/Commands/AddCategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PicoBoards.Forums.Commands
+{
+    public sealed class AddCategoryCommand : IValidatable
+    {
+        [Required]
+        public string Name { get; }
+
+        public AddCategoryCommand(string name)
+            => Name = name;
+    }
+}
---
+++ PicoBoards/Forums/ForumService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using PicoBoards.Forums.Commands;
+using Tortuga.Chain;
+
+namespace PicoBoards.Forums
+{
+    public sealed class ForumService
+    {
+        private readonly MySqlDataSource dataSource;
+
+        public ForumService(MySqlDataSource dataSource)
+            => this.dataSource = dataSource;
+
+        public async Task ExecuteAsync(AddCategoryCommand command)
+        {
+            if (!command.IsValid())
+                throw new CommandException("Invalid fields.");
+
+            await dataSource
+                .Insert("Category", command)
+                .ExecuteAsync();
+        }
+    }
+}
