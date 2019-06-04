06153bf98f7a56a48fbebfcf6111d5678a28d998
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-03 22:26:18 -0500

    Implement /Home/Index page

--- PicoBoards.Web/Features/Home/HomeController.cs
+++ PicoBoards.Web/Features/Home/HomeController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PicoBoards.Forums;
+using PicoBoards.Forums.Queries;
 
 namespace PicoBoards.Web.Features.Home
 {
     public class HomeController : Controller
     {
+        private readonly ForumService forumService;
+
+        public HomeController(ForumService forumService)
+            => this.forumService = forumService;
+
         [HttpGet]
-        public IActionResult Index() => View();
+        public async Task<IActionResult> Index()
+        {
+            var model = await forumService.QueryAsync(new AllCategoryDetailsQuery());
+            return View(model);
+        }
     }
 }
