65e6f4cf1357522e6e0844cc67b45ff6e683d0e1
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-03 22:36:22 -0500

    Add domain objects for forum pages

---
+++ PicoBoards/Forums/Models/ForumDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PicoBoards.Forums.Models
+{
+    public sealed class ForumDetails
+    {
+        public int ForumId { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public DateTime Created { get; }
+
+        public TopicListingCollection Topics { get; }
+
+        public ForumDetails(
+            int forumId,
+            string name,
+            string description,
+            DateTime created,
+            TopicListingCollection topics)
+        {
+            ForumId = forumId;
+            Name = name;
+            Description = description;
+            Created = created;
+            Topics = topics;
+        }
+    }
+}
---
+++ PicoBoards/Forums/Models/TopicListing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PicoBoards.Forums.Models
+{
+    public sealed class TopicListing
+    {
+        public int TopicId { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public bool IsLocked { get; }
+
+        public bool IsSticky { get; }
+
+        public TopicListing(
+            int topicId,
+            string name,
+            string description,
+            bool isLocked,
+            bool isSticky)
+        {
+            TopicId = topicId;
+            Name = name;
+            Description = description;
+            IsLocked = isLocked;
+            IsSticky = isSticky;
+        }
+    }
+
+    public sealed class TopicListingCollection : ICollection<TopicListing>
+    {
+        private readonly List<TopicListing> list;
+
+        public TopicListingCollection()
+            => list = new List<TopicListing>();
+
+        public TopicListingCollection(IEnumerable<TopicListing> collection)
+            => list = new List<TopicListing>(collection);
+
+        public int Count => list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(TopicListing item) => list.Add(item);
+        public void Clear() => list.Clear();
+        public bool Contains(TopicListing item) => list.Contains(item);
+        public void CopyTo(TopicListing[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
+        public IEnumerator<TopicListing> GetEnumerator() => list.GetEnumerator();
+        public bool Remove(TopicListing item) => list.Remove(item);
+        IEnumerator IEnumerable.GetEnumerator() => list.GetEnumerator();
+    }
+}
