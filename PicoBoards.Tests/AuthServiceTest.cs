55f329a60380979df9c04ebe6f515b0607149d66
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-04 15:33:41 -0500

    Reset database for every integration test

--- PicoBoards.Tests/AuthServiceTest.cs
+++ PicoBoards.Tests/AuthServiceTest.cs
@@ -1,5 +1,6 @@
-﻿using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using PicoBoards.Security.Authentication;
 using PicoBoards.Security.Authentication.Commands;
 using PicoBoards.Security.Authentication.Queries;
@@ -10,24 +11,42 @@ namespace PicoBoards.Tests
 {
     public class AuthServiceTest
     {
+        private async Task<MySqlDataSource> CreateDatabase()
+        {
+            var directory = Path.Combine(Environment.CurrentDirectory, "../../../../Database");
+            var schemaPath = Path.Combine(directory, "schema.sql");
+            var viewsPath = Path.Combine(directory, "views.sql");
+
+            var schemaText = await File.ReadAllTextAsync(schemaPath);
+            var viewsText = await File.ReadAllTextAsync(viewsPath);
+
+            var dataSource = new MySqlDataSource(Configuration.ConnectionStringWithoutDatabase);
+            await dataSource.Sql("DROP DATABASE IF EXISTS `PicoBoards`").ExecuteAsync();
+            await dataSource.Sql(schemaText).ExecuteAsync();
+            await dataSource.Sql(viewsText).ExecuteAsync();
+
+            return new MySqlDataSource(Configuration.ConnectionString);
+        }
+
         [Fact]
         public async Task SuccessfulCase()
         {
-            await SuccessfulCase_Impl("John_Smith@example1.com",         "John_Smith",         "password");
-            await SuccessfulCase_Impl("Michelle_Cooper@example2.com",    "Michelle_Cooper",    "password");
-            await SuccessfulCase_Impl("Theresa_Brown@example3.com",      "Theresa_Brown",      "password");
-            await SuccessfulCase_Impl("Joyce_Robinson@example4.com",     "Joyce_Robinson",     "password");
-            await SuccessfulCase_Impl("Lawrence_Jones@example5.com",     "Lawrence_Jones",     "password");
-            await SuccessfulCase_Impl("Frank_Sanchez@example6.com",      "Frank_Sanchez",      "password");
-            await SuccessfulCase_Impl("Phillip_Richardson@example7.com", "Phillip_Richardson", "password");
-            await SuccessfulCase_Impl("David_Bailey@example8.com",       "David_Bailey",       "password");
-            await SuccessfulCase_Impl("Jose_Parker@example9.com",        "Jose_Parker",        "password");
-            await SuccessfulCase_Impl("Ralph_Perez@example10.com",       "Ralph_Perez",        "password");
+            var dataSource = await CreateDatabase();
+            await SuccessfulCase_Impl(dataSource, "John_Smith@example1.com",         "John_Smith",         "password");
+            await SuccessfulCase_Impl(dataSource, "Michelle_Cooper@example2.com",    "Michelle_Cooper",    "password");
+            await SuccessfulCase_Impl(dataSource, "Theresa_Brown@example3.com",      "Theresa_Brown",      "password");
+            await SuccessfulCase_Impl(dataSource, "Joyce_Robinson@example4.com",     "Joyce_Robinson",     "password");
+            await SuccessfulCase_Impl(dataSource, "Lawrence_Jones@example5.com",     "Lawrence_Jones",     "password");
+            await SuccessfulCase_Impl(dataSource, "Frank_Sanchez@example6.com",      "Frank_Sanchez",      "password");
+            await SuccessfulCase_Impl(dataSource, "Phillip_Richardson@example7.com", "Phillip_Richardson", "password");
+            await SuccessfulCase_Impl(dataSource, "David_Bailey@example8.com",       "David_Bailey",       "password");
+            await SuccessfulCase_Impl(dataSource, "Jose_Parker@example9.com",        "Jose_Parker",        "password");
+            await SuccessfulCase_Impl(dataSource, "Ralph_Perez@example10.com",       "Ralph_Perez",        "password");
         }
 
-        private async Task SuccessfulCase_Impl(string emailAddress, string userName, string password)
+        private async Task SuccessfulCase_Impl(
+            MySqlDataSource dataSource, string emailAddress, string userName, string password)
         {
-            var dataSource = new MySqlDataSource(Configuration.ConnectionString);
             var authService = new AuthService(dataSource);
 
             var token = await authService
--- PicoBoards.Tests/Configuration.cs
+++ PicoBoards.Tests/Configuration.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace PicoBoards.Tests
 {
     public sealed class Configuration
     {
         public const string ConnectionString = "server=127.0.0.1;port=3306;database=picoboards;uid=root";
+        public const string ConnectionStringWithoutDatabase = "server=127.0.0.1;port=3306;uid=root";
     }
 }
