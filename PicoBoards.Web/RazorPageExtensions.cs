cf8b0c55e62b50968b52ec7dabb5fbaca3f0ee14
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-06-05 22:12:28 -0500

    Use partial views for navigation bar

---
+++ PicoBoards.Web/RazorPageExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace PicoBoards.Web
+{
+    public static class RazorPageExtensions
+    {
+        public static string GetControllerName(this RazorPage instance)
+            => instance.GetControllerActionDescriptor().ControllerName;
+
+        public static string GetActionName(this RazorPage instance)
+            => instance.GetControllerActionDescriptor().ActionName;
+
+        public static string GetId(this RazorPage instance)
+            => instance.GetControllerActionDescriptor().Id;
+
+        public static ControllerActionDescriptor GetControllerActionDescriptor(this RazorPage instance)
+            => (ControllerActionDescriptor) instance.ViewContext.ActionDescriptor;
+    }
+}
